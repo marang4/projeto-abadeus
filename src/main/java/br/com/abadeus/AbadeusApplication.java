c97b6b4975aaf8639122050e4304f8884d22f981
Gabriel Marangoni <gabrielmarangoni2@hotmail.com>
2026-02-18 20:52:37 -0300

    primeiro commit

---
+++ src/main/java/br/com/abadeus/AbadeusApplication.java
@@ -0,0 +1,13 @@
+package br.com.abadeus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbadeusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AbadeusApplication.class, args);
+	}
+
+}
---
+++ src/main/java/br/com/abadeus/infra/config/SwaggerConfiguration.java
@@ -0,0 +1,27 @@
+package br.com.abadeus.infra.config;
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfiguration {
+
+    @Bean
+    public OpenAPI customOpenAPI() {
+        return new OpenAPI().addSecurityItem(new SecurityRequirement().addList("bearerAuth"))
+                .components(new Components().addSecuritySchemes("bearerAuth",
+                        new SecurityScheme()
+                                .type(SecurityScheme.Type.HTTP)
+                                .scheme("bearer")
+                                .bearerFormat("JWT")))
+                .info(new Info()
+                        .title("Ticketing system Abadeus ")
+                        .version("1.0.0")
+                        .description("Sistema de venda de ingressos"));
+    }
+}
